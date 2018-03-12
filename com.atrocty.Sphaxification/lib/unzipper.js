f79cf09c14726978cabc9b15730bc528a9fbccaf
Timm Schütte <atrocty@gmx.de>
2018-03-12 15:35:17 +0100

    190312 Update

--- com.atrocty.Sphaxification/lib/unzipper.js
+++ com.atrocty.Sphaxification/lib/unzipper.js
@@ -1,58 +1,106 @@
-var objCsInterface 		= new CSInterface();
-// CEP7+ need absolute path, not relative
-var strDir 				= objCsInterface.getSystemPath(SystemPath.EXTENSION);
-const objJSZip 			= require(strDir + "/lib/jszip/");
-const objFS 			= require(strDir + "/lib/fs-extra/");
-const strValidFiletypes = [".png",".png.mcmeta"];
-var objZip 				= new objJSZip();
+/*----------------------------------
 
+Texture pack extractor & packer
+
+----------------------------------*/
+var objCsInterface = new CSInterface();
+// CEP7+ need absolute path, not relative
+var strDir = objCsInterface.getSystemPath(SystemPath.EXTENSION);
+// Required modules
+const objJSZip = require(strDir + "/lib/jszip/");
+const objFS = require(strDir + "/lib/fs-extra/");
+const strValidFiletypes = /(.png.mcmeta|.png)$/;
+var objZip = new objJSZip();
+// Unzipper
 $(function()
 {
 	$("#zipfile").change(function(event)
 	{
 		// Closure to capture the file information.
-		function handleFile(objFile)
+		function handleFile(objZipData)
 		{
-			objJSZip.loadAsync(objFile, {createFolders: true})
-			.then
-			(
-				function(objZip)
+			objJSZip.loadAsync(objZipData,
+			{
+				createFolders: true
+			})
+			.then(function(objZip)
+			{
+				var strPromises = Object.keys(objZip.files).filter(function(strFileName)
 				{
-					objZip.forEach(function(strRelativePath, objZipEntry)
+					debugAlert("NAME: " + strFileName);
+					// Only extract files in assets folder
+					if (!(/assets/g).test(strFileName))
 					{
-						objZipEntry.async('nodebuffer').then(
-							function(content) 
-							{
-								debugAlert(content);
-								var strDest = strDir + '/' + objZipEntry.name;
-								objFS.writeFileSync(strDest, content);
-							});
-						debugAlert(objZipEntry.name);
-					});
-				},
-				function(error)
+						return false;
+					}
+					// don't consider non image files
+					return strValidFiletypes.test(strFileName.toLowerCase());
+				}).map(function(strFileName)
 				{
-					debugAlert("Error reading " + objFile.name + ": " + error.message);
-				}
-			);
+					var objFile = objZip.files[strFileName];
+					return objFile.async("nodebuffer").then(function(blob)
+					{
+						strDest = strDir + '/' + strFileName;
+						objFS.writeFileSync(strDest, blob);
+						console.log(JSON.stringify(blob, null, 4));
+						debugAlert("BLOB: " + blob + " NAME: " + strFileName + URL.createObjectURL(blob));
+						return [
+							strFileName, // keep the link between the file name and the content
+							URL.createObjectURL(blob) // create an url. img.src = URL.createObjectURL(...) will work
+						];
+					});
+				});
+				return Promise.all(strPromises);
+			})
+			.then(function(result)
+			{
+				debugAlert("RESULTAT: " + result + typeof(result));
+				console.log(JSON.stringify(result, null, 4));
+				return result.reduce(function (acc, val) 
+				{
+					acc[val[0]] = val[1];
+					debugAlert("acc[val[0]]: " + acc[val[0]] + typeof(acc[val[0]]));
+					debugAlert("val[0]: " + val[0] + typeof(val[0]));
+					return acc;
+			  	},{});
+			})
+			.then(function(imgData)
+			{
+				var strDest = null;
+				debugAlert("imgData " + imgData + typeof(imgData));
+				console.log(JSON.stringify(imgData, null, 4));
+				console.log(imgData);
+//				for(var strName in imgData) 
+//				{
+//					strDest = strDir + '/' + strName;
+//					debugAlert("strName: " + strName + " strDest " + strDest);
+//					debugAlert("strName " + imgData[strName] + typeof(imgData[strName]));
+//					var content = objFS.readFileSync(imgData[strName]);
+//					console.log(content);
+//					objFS.writeFileSync(strDest, content);
+//					console.log(imgData);
+//				}
+			})
+				/*objZip.forEach(function(strRelativePath, objZipEntry)
+				{
+					objZipEntry.async('nodebuffer').then(
+						function(content)
+						{
+							debugAlert(content);
+							var strDest = strDir + '/' + objZipEntry.name;
+							objFS.writeFileSync(strDest, content);
+						});
+					debugAlert(objZipEntry.name);
+				});
+			},
+			function(error)
+			{
+				debugAlert("Error reading " + objFile.name + ": " + error.message);
+			}*/
 		}
-		debugAlert(event.target.files[0].name);
-		if (boolCorrectFiletype(event.target.files[0].name))
+		if (event.target.files[0].name.endsWith(".zip"))
 		{
-			handleFile(event.target.files[0]);	
+			handleFile(event.target.files[0]);
 		}
 	});
 })
-
-function boolCorrectFiletype(strPath)
-{
-	debugAlert(typeof(strPath));
-	for (var i = 0; i < strValidFiletypes.length; i++)
-	{
-//		if strPath.endsWith(strValidFiletypes[i])
-//		{
-//			return true;
-//		}
-	}
-	return false;
-}
