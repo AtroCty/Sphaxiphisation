7cbc041b49b351812861b5e5e43c6e2d33a51cf5
Timm Schütte <atrocty@gmx.de>
2018-06-14 12:33:09 +0200

    Fast commit test

--- com.atrocty.Sphaxification/lib/unzipper.js
+++ com.atrocty.Sphaxification/lib/unzipper.js
@@ -3,85 +3,86 @@
 Texture pack extractor & packer
 
 ----------------------------------*/
-var objCsInterface = new CSInterface();
+var objCsInterface = new CSInterface( );
 // CEP7+ need absolute path, not relative
-var strDir = objCsInterface.getSystemPath(SystemPath.EXTENSION);
+var strDir = objCsInterface.getSystemPath( SystemPath.EXTENSION );
 // Required modules
-const objJSZip = require(strDir + "/lib/jszip/");
-const objFS = require(strDir + "/lib/fs-extra/");
+const objJSZip = require( strDir + "/lib/jszip/" );
+const objFS = require( strDir + "/lib/fs-extra/" );
 const strValidFiletypes = /(.png.mcmeta|.png)$/;
-var objZip = new objJSZip();
+var objZip = new objJSZip( );
 // Unzipper
-$(function()
+$( function ( )
 {
-	$("#zipfile").change(function(event)
+	$( "#zipfile" ).change( function ( event )
 	{
 		// Closure to capture the file information.
-		function handleFile(objZipData)
+		function handleFile( objZipData )
 		{
-			objJSZip.loadAsync(objZipData,
-			{
-				createFolders: true
-			})
-			.then(function(objZip)
-			{
-				var strPromises = Object.keys(objZip.files).filter(function(strFileName)
+			objJSZip.loadAsync( objZipData,
 				{
-					debugAlert("NAME: " + strFileName);
-					// Only extract files in assets folder
-					if (!(/assets/g).test(strFileName))
+					createFolders: true
+				} )
+				.then( function ( objZip )
+				{
+					var strPromises = Object.keys( objZip.files ).filter( function ( strFileName )
 					{
-						return false;
-					}
-					// don't consider non image files
-					return strValidFiletypes.test(strFileName.toLowerCase());
-				}).map(function(strFileName)
-				{
-					var objFile = objZip.files[strFileName];
-					return objFile.async("nodebuffer").then(function(blob)
+						debugAlert( "NAME: " + strFileName );
+						// Only extract files in assets folder
+						if ( !( /assets/g ).test( strFileName ) )
+						{
+							return false;
+						}
+						// don't consider non image files
+						return strValidFiletypes.test( strFileName.toLowerCase( ) );
+					} ).map( function ( strFileName )
 					{
-						strDest = strDir + '/' + strFileName;
-						objFS.writeFileSync(strDest, blob);
-						console.log(JSON.stringify(blob, null, 4));
-						debugAlert("BLOB: " + blob + " NAME: " + strFileName + URL.createObjectURL(blob));
-						return [
-							strFileName, // keep the link between the file name and the content
-							URL.createObjectURL(blob) // create an url. img.src = URL.createObjectURL(...) will work
-						];
-					});
-				});
-				return Promise.all(strPromises);
-			})
-			.then(function(result)
-			{
-				debugAlert("RESULTAT: " + result + typeof(result));
-				console.log(JSON.stringify(result, null, 4));
-				return result.reduce(function (acc, val) 
+						var objFile = objZip.files[ strFileName ];
+						return objFile.async( "nodebuffer" ).then( function ( blob )
+						{
+							strDest = strDir + '/' + strFileName;
+							objFS.writeFileSync( strDest, blob );
+							console.log( JSON.stringify( blob, null, 4 ) );
+							debugAlert( "BLOB: " + blob + " NAME: " + strFileName + URL.createObjectURL( blob ) );
+							return [
+								strFileName, // keep the link between the file name and the content
+								URL.createObjectURL( blob ) // create an url. img.src = URL.createObjectURL(...) will work
+							];
+						} );
+					} );
+					return Promise.all( strPromises );
+				} )
+				.then( function ( result )
 				{
-					acc[val[0]] = val[1];
-					debugAlert("acc[val[0]]: " + acc[val[0]] + typeof(acc[val[0]]));
-					debugAlert("val[0]: " + val[0] + typeof(val[0]));
-					return acc;
-			  	},{});
-			})
-			.then(function(imgData)
-			{
-				var strDest = null;
-				debugAlert("imgData " + imgData + typeof(imgData));
-				console.log(JSON.stringify(imgData, null, 4));
-				console.log(imgData);
-//				for(var strName in imgData) 
-//				{
-//					strDest = strDir + '/' + strName;
-//					debugAlert("strName: " + strName + " strDest " + strDest);
-//					debugAlert("strName " + imgData[strName] + typeof(imgData[strName]));
-//					var content = objFS.readFileSync(imgData[strName]);
-//					console.log(content);
-//					objFS.writeFileSync(strDest, content);
-//					console.log(imgData);
-//				}
-			})
-				/*objZip.forEach(function(strRelativePath, objZipEntry)
+					debugAlert( "RESULTAT: " + result + typeof ( result ) );
+					console.log( JSON.stringify( result, null, 4 ) );
+					return result.reduce( function ( acc, val )
+					{
+						acc[ val[ 0 ] ] = val[ 1 ];
+						debugAlert( "acc[val[0]]: " + acc[ val[ 0 ] ] + typeof ( acc[ val[ 0 ] ] ) );
+						debugAlert( "val[0]: " + val[ 0 ] + typeof ( val[ 0 ] ) );
+						return acc;
+					},
+					{} );
+				} )
+				.then( function ( imgData )
+				{
+					var strDest = null;
+					debugAlert( "imgData " + imgData + typeof ( imgData ) );
+					console.log( JSON.stringify( imgData, null, 4 ) );
+					console.log( imgData );
+					// for(var strName in imgData) 
+					// {
+					// 	strDest = strDir + '/' + strName;
+					// 	debugAlert("strName: " + strName + " strDest " + strDest);
+					// 	debugAlert("strName " + imgData[strName] + typeof(imgData[strName]));
+					// 	var content = objFS.readFileSync(imgData[strName]);
+					// 	console.log(content);
+					// 	objFS.writeFileSync(strDest, content);
+					// 	console.log(imgData);
+					// }
+				} )
+			/*objZip.forEach(function(strRelativePath, objZipEntry)
 				{
 					objZipEntry.async('nodebuffer').then(
 						function(content)
@@ -98,9 +99,9 @@ $(function()
 				debugAlert("Error reading " + objFile.name + ": " + error.message);
 			}*/
 		}
-		if (event.target.files[0].name.endsWith(".zip"))
+		if ( event.target.files[ 0 ].name.endsWith( ".zip" ) )
 		{
-			handleFile(event.target.files[0]);
+			handleFile( event.target.files[ 0 ] );
 		}
-	});
-})
+	} );
+} )
