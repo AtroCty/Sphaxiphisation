df60e71075c0d868fcb34008cda0cb7439daefb5
Timm Schütte <atrocty@gmx.de>
2018-06-18 21:50:58 +0200

    update

--- com.atrocty.Sphaxification/lib/navbar.js
+++ com.atrocty.Sphaxification/lib/navbar.js
@@ -1,42 +1,41 @@
 // Loop through the buttons and add the active class to the current/clicked button
-function clickButton(buttonID)
+function clickButton( buttonID )
 {
-	var btns = document.getElementById("buttons").getElementsByTagName("a");
-	
-	
-	if (btns[buttonID].className !== "active") 
+	var btns = document.getElementById( "buttons" ).getElementsByTagName( "a" );
+
+	if ( btns[ buttonID ].className !== "active" )
 	{
 		// Clear all buttons
-		for (var i = 0; i < btns.length; i++)
+		for ( var i = 0; i < btns.length; i++ )
 		{
-			btns[i].className = "";
+			btns[ i ].className = "";
 		}
-		
-		btns[buttonID].className = "active";
+
+		btns[ buttonID ].className = "active";
 	}
-	
-	if (buttonID === 3)
+
+	if ( buttonID === 3 )
 	{
-		if (typeof(cep_node) !== 'undefined')
+		if ( typeof ( cep_node ) !== 'undefined' )
 		{
 			//if require and process is available, it should be mixed context
-			if ((typeof(require) !== 'undefined') && (typeof(process) !== 'undefined')) 
+			if ( ( typeof ( require ) !== 'undefined' ) && ( typeof ( process ) !== 'undefined' ) )
 			{
-				$("#result2").val("Node.js is enabled with mixed-context mode");
+				$( "#result2" ).val( "Node.js is enabled with mixed-context mode" );
 			}
 			else
 			{
-			 	$("#result2").val("Node.js is enabled");
+				$( "#result2" ).val( "Node.js is enabled" );
 			}
 		}
-		else 
+		else
 		{
-			$("#result2").val("Node.js is disabled");
+			$( "#result2" ).val( "Node.js is disabled" );
 		}
 	}
-	
-	if (buttonID === 2)
+
+	if ( buttonID === 2 )
 	{
-		ziptester();
+		ziptester( );
 	}
 }
